1104ab08bd4cd94d9c00bbae3ef02415f026d1fe
AISG <pandaxel117@gmail.com>
2020-06-13 12:41:50 -0500

    Initial commit

---
+++ src/main/java/com/axel/MineriaFinal/MineriaFinalApplication.java
@@ -0,0 +1,13 @@
+package com.axel.MineriaFinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MineriaFinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MineriaFinalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/axel/MineriaFinal/MineriaFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.axel.MineriaFinal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MineriaFinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
